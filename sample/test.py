e6f1f7e27ef932e8a787875527cdff78189a1de9
viny <vincent.fontanella@linkio.net>
2023-10-10 17:24:35 +0200

    stucture project as paskage and remove folder from the yaml file

--- sample/test.py
+++ sample/test.py
@@ -1,7 +1,13 @@
+# sample/test;py
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from src.releasereporender import ReleaseRepoRender
    
 def main():
-    rrr = ReleaseRepoRender("3r.yml")
+    rrr = ReleaseRepoRender("3r.yml", "/home/viny/Documents/VinyTech/Rrr")
     rrr.prepare_repositories()
 
 if __name__ == '__main__':
