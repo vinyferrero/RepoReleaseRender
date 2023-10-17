e5d78632f8a35a847c78c1f3c68580a391f1fc7e
viny <vincent.fontanella@linkio.net>
2023-10-17 16:47:18 +0200

    add information about the project

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="rrr",
     version="0.0.1",
+    description= "Tool generating an environment for a release depending on multiproject source code",
+    author="Vincent Fontanella",
+    license="MIT",
+    url="https://github.com/vinyferrero/RepoReleaseRender",
     packages=find_packages(),
     install_requires=[
         "PyYAML>=6.0.1",
